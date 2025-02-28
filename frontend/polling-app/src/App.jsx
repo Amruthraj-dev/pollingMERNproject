9b2f87920ae9f8239540a5cd45b4a30760134812
Amruthraj <likhithakatla1002.gmail.com>
2025-02-28 18:08:47 +0530

    paths updated

--- frontend/polling-app/src/App.jsx
+++ frontend/polling-app/src/App.jsx
@@ -1,52 +1,58 @@
-import React from 'react' 
-import { BrowserRouter as Router,Routes,Route,Navigate } from 'react-router-dom'
-import Home from '../pages/Dashboard/Home'
-import LoginForm from '../pages/Auth/LoginForm'
-import SignUpForm from '../pages/Auth/SignUpForm'
-import CreatePoll from '../pages/Dashboard/CreatePoll'
-import Mypolls from '../pages/Dashboard/Mypolls'
-import VotedPolls from '../pages/Dashboard/VotedPolls'
-import Bookmarks from '../pages/Dashboard/Bookmarks'
-import UserProvider from './context/UserContext'
-import {Toaster} from 'react-hot-toast';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import Home from "../pages/Dashboard/Home";
+import LoginForm from "../pages/Auth/LoginForm";
+import SignUpForm from "../pages/Auth/SignUpForm";
+import CreatePoll from "../pages/Dashboard/CreatePoll";
+import Mypolls from "../pages/Dashboard/Mypolls";
+import VotedPolls from "../pages/Dashboard/VotedPolls";
+import Bookmarks from "../pages/Dashboard/Bookmarks";
+import UserProvider from "./context/UserContext";
+import { Toaster } from "react-hot-toast";
 
 const App = () => {
   return (
     <div>
-      <UserProvider >
-     <Router>
-      <Routes>
-        <Route path="/" element={<Root/>}/>
-        <Route path='/login' exact element={<LoginForm/>}/>
-        <Route path='/signup' exact element={<SignUpForm/>}/>
-        <Route path='/dashboard' exact element={<Home/>}/>
-        <Route path='/create-poll' exact element={<CreatePoll/>}/>
-        <Route path='/my-polls' exact element={<Mypolls/>}/>
-        <Route path='/voted-polls' exact element={<VotedPolls/>}/>
-        <Route path='/bookmarked-polls' exact element={<Bookmarks/>}/>
-      </Routes>
-     </Router>
+      <UserProvider>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Root />} />
+            <Route path="/login" exact element={<LoginForm />} />
+            <Route path="/signup" exact element={<SignUpForm />} />
+            <Route path="/dashboard" exact element={<Home />} />
+            <Route path="/create-poll" exact element={<CreatePoll />} />
+            <Route path="/my-polls" exact element={<Mypolls />} />
+            <Route path="/voted-polls" exact element={<VotedPolls />} />
+            <Route path="/bookmarked-polls" exact element={<Bookmarks />} />
+          </Routes>
+        </Router>
 
-     <Toaster toastOptions={{className:"",
-      style:{
-        fontSize:"13px"
-      }
-     }}/>
-     </UserProvider>
+        <Toaster
+          toastOptions={{
+            className: "",
+            style: {
+              fontSize: "13px",
+            },
+          }}
+        />
+      </UserProvider>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
 
+const Root = () => {
+  const isAuthenticated = !!localStorage.getItem("token");
 
-const Root=()=>{
-  const isAuthenticated=!!localStorage.getItem("token");
-
-  return isAuthenticated?(
-    <Navigate to="/dashboard"/>
-  ):
-  (
-    <Navigate to="login"/>
-  )
-}
+  return isAuthenticated ? (
+    <Navigate to="/dashboard" />
+  ) : (
+    <Navigate to="login" />
+  );
+};
